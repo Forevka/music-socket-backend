834976a1604c0fdf4a907ebc4d15eac444eb6b7e
OneZeroZeroOneOne <intheendreallife@gmail.com>
2019-07-30 14:49:16 +0300

    osia

--- client/websocket-client.py
+++ client/websocket-client.py
@@ -27,15 +27,17 @@ async def hello():
     my_channel = {}
     async with websockets.connect(uri) as websocket:
         await websocket.send(create_request(PossibleEvents.Login, {"username": "admin", "password": "admid"}))
+        await websocket.send(create_request(PossibleEvents.GetChannels))
         print(f"> waiting for info from server")
         me = await websocket.recv()
         print(f"> you logged as {me}")
         while True:
             events_list = '\n'.join([f"{n + 1} - {i.name}" for n, i in enumerate(list(PossibleEvents))])
             r = input(f"What to send\n{events_list}\n>")
-            l = str(r).split(" ")
-            logger.info(l)
-            await websocket.send(create_request(list(PossibleEvents)[int(l[0]) - 1], int(l[1])))
+            if r == 6:
+                l = input("Please read channel")
+                await websocket.send(create_request(list(PossibleEvents)[int(r) - 1], int(l)))
+            await websocket.send(create_request(list(PossibleEvents)[int(r) - 1]))
             server_sent = await websocket.recv()
             print(f"> Server sent {server_sent}")
             #await asyncio.sleep(1)
