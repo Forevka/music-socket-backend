ccc45d20fc81b0da002790075102f718350da465
Forevka <starfield@i.ua>
2019-08-18 18:30:56 +0300

    CORS

--- server/http_server/http_server.py
+++ server/http_server/http_server.py
@@ -2,6 +2,7 @@ import asyncio
 from aiohttp import web
 from loguru import logger
 from handlers import Handlers
+import aiohttp_cors
 import jwt
 
 
@@ -17,16 +18,36 @@ async def check_token(request, handler):
         return await handler(decoded, request)
     except:
         logger.debug("invalid token - {}".format(token))
-        return web.StreamResponse(status=401, reason=None)
+        return web.StreamResponse(status=401, reason=None, headers = {'Access-Control-Allow-Origin': "*",
+                    'test': '123',
+                    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS'})
 
+
+def register_route(app, path, method, handler):
+    resource = cors.add(app.router.add_resource(path))
+    cors.add(
+        resource.add_route(method, handler), {
+            "*": aiohttp_cors.ResourceOptions(
+                allow_credentials=True,
+                expose_headers="*",
+                allow_headers="*"
+            )
+        })
 
 
 if __name__ == '__main__':
     app = web.Application()
-    hndl = Handlers()
     app2 = web.Application(middlewares=[check_token])
-    app.router.add_route('GET',  '/add_new_user', hndl.hendler_add_new_user)
-    app2.router.add_route('GET',  '/get_user', hndl.hendler_get_user)
-    app.router.add_route('GET',  '/authentication', hndl.hendler_authentication)
+
+    hndl = Handlers()
+    cors = aiohttp_cors.setup(app)
+
+
+    register_route(app, '/add_new_user', 'POST', hndl.hendler_add_new_user)
+    register_route(app, '/authentication', 'POST', hndl.hendler_authentication)
+
+    register_route(app2, '/get_user', 'POST', hndl.hendler_get_user)
     app.add_subapp('/methods/', app2)
+
+
     web.run_app(app, host= "localhost", port=443)
